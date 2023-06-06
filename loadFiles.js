acdf00b3f3aa4917adaa4763b3b31c683e4b5a79
louispaulet <louispaulet13@gmail.com>
2023-06-06 16:33:17 +0200

    refactor: split main html into a list of html project files that are loaded using js into the main index.html file

---
+++ loadFiles.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  // Array of file names
+  var files = [
+    'project1.html',
+    'project2.html',
+    'project3.html',
+    'project4.html',
+    'project5.html',
+    'project6.html',
+    'project7.html',
+    'project8.html',
+    'project9.html'
+  ];
+
+  // Function to load HTML file
+  function loadFile(file) {
+    $.ajax({
+      url: file,
+      dataType: 'html',
+      success: function(data) {
+        $('#' + file.split('.')[0]).html(data);
+      }
+    });
+  }
+
+  // Load each HTML file
+  for (var i = 0; i < files.length; i++) {
+    loadFile(files[i]);
+  }
+});
